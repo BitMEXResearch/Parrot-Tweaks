9a2d7c758d60486dd6f4e59bb88a81ff95b3a2a3
Mohammed Israil <mdisrail2468@gmail.com>
2020-06-17 04:35:17 +0530

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -8,6 +8,8 @@ sudo apt-get install python3-pip
 sudo pip install virtualenv
 sudo pip3 install virtualenv
 
+sudo apt-get install jq
+
 #[sublime-text-3](http://www.webupd8.org/2013/07/sublime-text-3-ubuntu-ppa-now-available.html)
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
